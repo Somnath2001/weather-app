55df6e70623c39610ba8a465808ae71a149f0a69
somnathdudhat <somnathdudhat222@gmail.com>
2021-09-26 00:47:39 +0530

    done

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,7 @@ const getInfo = async (event) => {
     datahide.classList.add("data_hide");
   } else {
     try {
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=${API_KEY}`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=${API_KEY}`;
       const response = await fetch(url);
       const data = await response.json();
 
