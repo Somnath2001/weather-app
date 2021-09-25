9419ef11a4b4457ec3614d9b216dc439368018d4
somnathdudhat <somnathdudhat222@gmail.com>
2021-09-25 23:57:51 +0530

    update

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,6 @@ const getInfo = async (event) => {
       const data = await response.json();
 
       const arrData = [data];
-      console.log(arrData);
       city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`;
       temp_rel_value.innerText = arrData[0].main.temp;
 
@@ -48,3 +47,31 @@ const getInfo = async (event) => {
   }
 };
 submitbtn.addEventListener("click", getInfo);
+
+const getCurrentDay = () => {
+  let weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  let currentTime = new Date();
+  days = weekday[currentTime.getDay()];
+  let day = document.getElementById("day");
+  day.innerText = days;
+};
+
+getCurrentDay();
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+var yyyy = today.getFullYear();
+
+today = dd + "/" + mm + "/" + yyyy;
+
+let today_date = document.getElementById("today_date");
+today_date.innerText = today;
